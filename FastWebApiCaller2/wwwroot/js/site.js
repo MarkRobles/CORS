82ff7f3dc157c8823cb87ffaf5933cc13bc4e3af
marcos.robles <mirh92@outlook.com>
2022-07-03 17:19:53 -0500

    Add Test Projects

---
+++ FastWebApiCaller2/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+function reqListener () {
+    console.log(this.responseText);
+  }
+  
+  var oReq = new XMLHttpRequest();
+  oReq.addEventListener("load", reqListener);
+  oReq.open("GET", "https://app-aliveblog-qa-scus-001.azurewebsites.net/api/v1/publicaciones");
+  oReq.send();
+  
