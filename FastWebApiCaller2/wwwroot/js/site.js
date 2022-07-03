56dcf942631fcdc89e93bdaf29920d1ebd889008
marcos.robles <mirh92@outlook.com>
2022-07-03 18:24:44 -0500

    Preflighted requests

--- FastWebApiCaller2/wwwroot/js/site.js
+++ FastWebApiCaller2/wwwroot/js/site.js
@@ -6,8 +6,42 @@ function reqListener () {
     console.log(this.responseText);
   }
   
+
+  //Using XMLHttpRequest to make api calls
   var oReq = new XMLHttpRequest();
   oReq.addEventListener("load", reqListener);
   oReq.open("GET", "https://app-aliveblog-qa-scus-001.azurewebsites.net/api/v1/publicaciones");
   oReq.send();
   
+
+
+var invocation = new XMLHttpRequest();
+//invocation.addEventListener("load", reqListener);
+var url = "https://app-aliveblog-qa-scus-001.azurewebsites.net/api/v1/publicaciones";
+var body = `{
+  "titulo": "Obras de Patiselli",
+  "descripcion": "Judith killing someone",
+  "comentarios": [
+    {
+      "descripcion": "I just forgot who she killed"
+    }
+  ]
+}`;
+
+
+
+
+//Solicitudes Verificadas
+//https://developer.mozilla.org/es/docs/Web/HTTP/CORS#solicitudes_verificadas
+
+
+  if(invocation)
+  {
+  
+      invocation.open('POST', url, true);
+      invocation.setRequestHeader('X-PINGOTHER', 'pingpong');
+      invocation.setRequestHeader('Content-Type', 'application/json');
+      invocation.onreadystatechange = reqListener;
+      invocation.send(body);
+    }
+
